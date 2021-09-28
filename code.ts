bc92ae92bd9a88701019aea0c98ae3038b89b2d0
nick <32414396+qurle@users.noreply.github.com>
2021-09-29 00:27:51 +0300

    commit, innit?

---
+++ code.ts
@@ -0,0 +1,70 @@
+// I really don't care about quality of the code, but you can fork it to make it better
+
+const finishMsgs = ["Done!", "You got it!", "Shufffffled!", "Is that all?", "My job here is done", "Gotcha!", "It wasn't hard", "Got it! What's next?"]
+
+let notification: NotificationHandler
+let count: number
+let selection: ReadonlyArray<SceneNode>
+
+let working: boolean
+
+interface Position {
+  x: number
+  y: number
+}
+
+figma.on("currentpagechange", escape)
+
+// Main + Elements Check
+const nodes: SceneNode[] = [];
+working = true
+selection = figma.currentPage.selection
+if (selection) {
+  count = selection.length
+  if (count > 1) {
+    shuffle(selection)
+    working = false
+    finish()
+  } else {
+    if (count === 1) {
+      if (selection[0].type === "GROUP") {
+        shuffle(selection[0].children)
+      } else
+        notify("1 layer is succesfully shuffled with itself")
+    }
+    if (count < 1)
+      notify("You need some layers selected to shuffle them")
+    figma.closePlugin()
+  }
+}
+
+function shuffle(s: ReadonlyArray<SceneNode>) {
+  let res: Array<Position> = s.map(el => ({ x: el.x, y: el.y }))
+  for (let i = res.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [res[i], res[j]] = [res[j], res[i]];
+    [s[i].x, s[i].y] = [res[i].x, res[i].y]
+  }
+  [s[0].x, s[0].y] = [res[0].x, res[0].y]
+}
+
+function notify(text: string) {
+  if (notification != null)
+    notification.cancel()
+  notification = figma.notify(text)
+}
+
+function escape() {
+  if (notification != null)
+    notification.cancel()
+  if (working) {
+    notify("Plugin work have been interrupted")
+  }
+}
+
+function finish() {
+  working = false
+  notify(finishMsgs[Math.floor(Math.random() * finishMsgs.length)])
+  //figma.viewport.scrollAndZoomIntoView(selection)
+  figma.closePlugin()
+}
---
+++ readme.md
@@ -0,0 +1,27 @@
+#Shuffle for Figma
+Randomize your grid! This plugin visually reorders your layers.
+![decorative](https://github.com/qurle/shuffle/blob/main/cover.png?raw=true)
+
+## How to use
+
+**Just select some layers** **and use it **via Quick Actions or Plugin menu. If you don't like the result, press Cmd/Ctrl+Alt+P a few times.
+
+_If you select only one group, the plugin will shuffle its contents._
+
+## **How it works**
+
+The plugin will store layers' positions, mix them and reassign to selected layers. Shuffle is perfect for grid-aligned items. However, it works with everything.
+
+## **Roadmap**
+
+Shuffle is simple, fast and non-disruptive. It just does its work. If you still have any ideas on how to make it better, leave a comment or contact me.
+
+## **Problem? Idea? Kind words?**
+
+You can contact me via e-mail at [nick.denq@gmail.com](mailto:nick.denq@gmail.com?subject=Spotify%20to%20Figma).
+
+But the most rapid way is [Telegram](http://t.me/qurle).
+
+Don't hesitate, I'd like to know your thoughts on this plugin (or we can talk about anything else).
+
+## **<3**
